ce299a98521d8041e666601916e6c6f8d372d6bd
Maung Ko Ko <mwkoko2020@gmail.com>
2023-06-05 17:52:23 -0600

    Complete Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [imageUrl, setImageUrl] = useState(null);
+
+  useEffect(() => {
+    const fetchDogImage = async () => {
+      try {
+        const response = await fetch('https://dog.ceo/api/breeds/image/random');
+        const data = await response.json();
+        setImageUrl(data.message);
+      } catch (error) {
+        console.error('Error fetching dog image:', error);
+      }
+    };
+
+    fetchDogImage();
+  }, []);
+
+  return (
+    <div>
+      {imageUrl ? <img src={imageUrl} alt="A Random Dog" /> : <p>Loading...</p>}
+    </div>
+  );
+}
+
+export default App;
